bcb492f2e314a595649449c40dfd6da61fe6e479
tcpruffman <haucke.raphael@protonmail.com>
2021-12-31 16:41:32 +0100

    start of insertion models

--- insertions/models.py
+++ insertions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your models here.
+class Offer(models.Model):
+    owner_id = models.ForeignKey(User, related_name='offer', on_delete=models.CASCADE)
+
+class Request(models.Model):
+    owner_id = models.ForeignKey(User, related_name='request', on_delete=models.CASCADE)
