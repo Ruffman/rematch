2fe2507a10f81ad23c149e6bdfde975a1b59fcf9
tcpruffman <haucke.raphael@protonmail.com>
2022-01-02 19:19:45 +0100

    added overview page added links to it from base sidebar

--- insertions/urls.py
+++ insertions/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 from . import views
 
-appname = 'insertions'
+app_name = 'insertions'
 urlpatterns = [
     path('insert/', views.InsertView.as_view(), name='insert'),
+    path('overview/', views.OverviewView.as_view(), name='overview'),
 ]
--- insertions/views.py
+++ insertions/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class InsertView(TemplateView):
     template_name = 'insert.html'
+
+class OverviewView(TemplateView):
+    template_name = 'overview.html'
