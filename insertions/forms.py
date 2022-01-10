f9c78cdb52b4177acead2e95aaab7222d05f6c8e
tcpruffman <haucke.raphael@protonmail.com>
2022-01-10 21:26:54 +0100

    new model for insertions

---
+++ insertions/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . import models
+
+class InsertionForm(forms.ModelForm):
+    class Meta:
+        fields = ("user_id")
+        model = models.Offer
+
+    user_id = forms.CharField(max_length=100)
