a123cda813192e51e2b3ce5ac9b001c0aa416ccd
tcpruffman <haucke.raphael@protonmail.com>
2022-01-15 22:47:52 +0100

    removed obsolete insert view and spilt it into request and offer view

--- insertions/urls.py
+++ insertions/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 app_name = 'insertions'
 urlpatterns = [
-    path('insert/', views.InsertView.as_view(), name='insert'),
     path('new_offer/', views.NewOfferView.as_view(), name='new_offer'),
+    path('new_request/', views.NewRequestView.as_view(), name='new_request'),
     path('overview/', views.OverviewView.as_view(), name='overview'),
-    path('detail/<int:id>', views.DetailView.as_view(), name='detail'),
+    path('detail/<int:id>', views.InsertionDetailView.as_view(), name='detail'),
 ]
