b1a75c3e135748e44f0d815565aee2978330f900
tcpruffman <haucke.raphael@protonmail.com>
2022-01-16 13:31:31 +0100

    matching models first draft

--- matching/models.py
+++ matching/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
+
+from insertions import Offer, Request
+
 # Create your models here.
+class Match(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+
+
+
+class OfferSentLikes(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+
+
+
+class OfferReceivedLikes(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+
+
+
+class RequestSentLikes(models.Model):
+request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+
+
+
+class RequestReceivedLikes(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
