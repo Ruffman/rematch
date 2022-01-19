fde05542602cfa0b926c63c90a8ecae0619c8b1a
tcpruffman <haucke.raphael@protonmail.com>
2022-01-19 14:43:16 +0100

    tag library for project wide tags

--- rematch/settings.py
+++ rematch/settings.py
@@ -68,6 +68,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'project_tags': 'rematch.templatetags.common_tags',
+            },
         },
     },
 ]
