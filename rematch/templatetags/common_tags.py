b3f897f8e9098145c229d2e363bb951be1cd0c90
tcpruffman <haucke.raphael@protonmail.com>
2022-02-01 21:58:12 +0100

    added tags to find model fields in templates

--- rematch/templatetags/common_tags.py
+++ rematch/templatetags/common_tags.py
@@ -6,3 +6,35 @@ register = template.Library()
 @register.filter
 def verbose_name(obj):
     return obj._meta.verbose_name
+
+
+@register.simple_tag
+def model_name(value):
+    """
+    Django template filter which returns the verbose name of a model.
+    """
+    # if hasattr(value, "model"):
+    #     value = value.model
+
+    return value._meta.verbose_name.title()
+
+
+@register.simple_tag
+def model_name_plural(value):
+    """
+    Django template filter which returns the plural verbose name of a model.
+    """
+    # if hasattr(value, "model"):
+    #     value = value.model
+
+    return value._meta.verbose_name_plural.title()
+
+
+@register.simple_tag
+def field_name(obj, field):
+    """
+    Django template filter which returns the verbose name of an object's,
+    model's or related manager's field.
+    """
+
+    return obj._meta.get_field(field).verbose_name.title()
