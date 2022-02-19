b81eaf3a08c7538083ed0205fad6651476a78b90
tcpruffman <haucke.raphael@protonmail.com>
2022-02-19 20:42:52 +0100

    registered some models in admin

--- insertions/admin.py
+++ insertions/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+admin.site.register(models.Offer)
+admin.site.register(models.Request)
+admin.site.register(models.Finance_Type)
+admin.site.register(models.Heating_Type)
 admin.site.register(models.Object_Type)
