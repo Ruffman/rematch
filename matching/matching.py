da31c3082bc16730fe6f342cfe12d69d44e1ae7a
tcpruffman <haucke.raphael@protonmail.com>
2022-02-19 21:46:46 +0100

    crude matching "algo"

--- matching/matching.py
+++ matching/matching.py
@@ -1,20 +1,32 @@
 # here all functionality for finding matches will go
 from insertions.models import Offer, Request
+from matching.models import Proposed_Match
 
 
 
-def lookup_matching_offers(request):
-    print("lookup matching offer")
+def generate_proposed_matches_for_request(instance):
+    print('generate_proposed_matches_for_request')
+    matches_query = Offer.objects.filter(object_type_id = instance.object_type_id) # TODO: here needs to go a proper matching algo
+    for match in matches_query:
+        Proposed_Match.objects.create(offer=match, request=instance)
+
+def generate_proposed_matches_for_offer(instance):
+    print('generate_proposed_matches_for_offer')
+    matches_query = Request.objects.filter(object_type_id = instance.object_type_id) # TODO: here needs to go a proper matching algo
+    for match in matches_query:
+        Proposed_Match.objects.create(offer=instance, request=match)
+
+def lookup_matches_for_request(instance):
+    generate_proposed_matches_for_request(instance)
 
 
-
-def lookup_matching_requests(offer):
-    print("lookup matching requests")
+def lookup_matches_for_offer(instance):
+    generate_proposed_matches_for_offer(instance)
 
 
 def lookup_matches(instance):
-    if instance.__class__ == Request:
-        lookup_matching_offers(instance)
+    if instance.__class__.__name__ == "Request":
+        lookup_matches_for_request(instance)
 
-    elif instance.__class__ == Offer:
-        lookup_matching_requests(instance)
+    elif instance.__class__.__name__ == "Offer":
+        lookup_matches_for_offer(instance)
